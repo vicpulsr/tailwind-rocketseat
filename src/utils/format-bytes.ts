e987bd8985ef3e1b8f53e0df1c2e1e88ed82842d
vicpulsr <vicke.puls@hotmail.com>
2024-06-27 11:24:02 -0300

    ✨ feat: test of function format-bytes

--- src/utils/format-bytes.ts
+++ src/utils/format-bytes.ts
@@ -1,10 +1,16 @@
 export function formatBytes(bytes: number) {
     const units = ["B", "KB", "MB", "GB", "TB", "PB"];
+
+    if (typeof bytes !== 'number' || isNaN(bytes)) {
+        throw new Error("Input must be a valid number");
+    }
+
+    if (bytes === 0) return `0 B`;
     
     let value = bytes;
     let unitIndex = 0;
 
-    while (value > 1024 && unitIndex < units.length - 1) {
+    while (value >= 1024 && unitIndex < units.length - 1) {
         value /= 1024;
         unitIndex++;
     }
