e96f6a3a35614fbae4f18bbc6a6079f0e841a0d9
vicpulsr <vicke.puls@hotmail.com>
2024-06-27 11:38:57 -0300

    ✨ feat: add coverage jest

--- jest.config.ts
+++ jest.config.ts
@@ -9,6 +9,7 @@ const createJestConfig = nextJest({
 // Add any custom config to be passed to Jest
 const config: Config = {
   coverageProvider: 'v8',
+  coverageDirectory: './src/jest/coverage',
   testEnvironment: 'jsdom',
   // Add more setup options before each test is run
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
